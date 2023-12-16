915ec0db2aa345de44544ddbd9bc25372fba9446
Brendan <daly2b@gmail.com>
2023-12-15 21:39:32 -0500

    Added more JavaScript

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var cities = [];
+var cities = [3,11,19,27,35];
 
 var searchCity = "";
 
@@ -15,15 +15,13 @@ var currTemp = $('.currTemp');
 var currWind = $('.currWind');
 var currHumidity = $('.currHumid');
 var cityName = '';
-//var lat = 40.981613;
-//var lon = -73.691925;
-function updateCity() {
-    if (searchCity) {
-        cityTitle.text(searchCity);
-    }
-}
+var currIcon = $('.currIcon');
 
-
+var dayOne = $('.dayOne');
+var dayTwo = $('.dayTwo');
+var dayThree = $('.dayThree');
+var dayFour = $('.dayFour');
+var dayFive = $('.dayFive');
 
 
 
@@ -63,11 +61,33 @@ async function currentWeather() {
       })
       .then((data) => {
         cityTitle.text(cityName);
+        currIcon.attr("src", `http://openweathermap.org/img/wn/${data.icon}.png`);
         currTemp.text('Temp: ' + data.main.temp + 'Fº');
         currWind.text('Wind: ' + data.wind.speed + 'MPH');
         currHumidity.text('Humidity: ' + data.main.humidity + '%');
         //console.log(data.list[0].main.temp);
       });
+      requestUrl =  `https://api.openweathermap.org/data/2.5/forecast?lat=${location.lat}&lon=${location.lon}&appid=${apiKey}&units=imperial`;
+      fetch(requestUrl)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data.list);
+          var index = 3;
+          for (var i = 0; i < 5; i ++) {
+            var day = $(`#${i}`);
+            console.log(day.children(".date"));
+            var onDay = data.list[index];
+            console.log(onDay.weather[0].icon);
+            day.children("#date").text(onDay.dt_txt);
+            day.children("#weatherIcon").attr("src", `http://openweathermap.org/img/wn/${onDay.weather[0].icon}.png`);
+            day.children("#temp").text('Temp: ' + onDay.main.temp + 'ºF');
+            day.children("#wind").text('Wind: ' + onDay.wind.speed + 'MPH');
+            day.children("#humidity").text('Humidity: ' + onDay.main.humidity + '%');
+            index += 8;
+          }
+        });
 };
 
 
@@ -77,16 +97,8 @@ async function currentWeather() {
 
 
 async function fiveDayWeather() {
-    //await getLocation();
-    var requestUrl =  `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
-    fetch(requestUrl)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data.list);
-        //console.log(data.list[0].main.temp);
-      });
+    let location = await getLocation();
+    
 };
 
 var cityList = $('.cityList');
@@ -97,7 +109,7 @@ function search(event) {
     currentWeather();
     searchBox.val("");
     console.log(cityName);
-    var newCity = $('<p>').text(cityName);
+    var newCity = $(`<p class="button" id="${cityName}">`).text(cityName);
     cityList.append(newCity);
 }
 
